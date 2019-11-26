681dfab1b1f2809118042a43a36c89d51572e5eb
johan enrique alcala este <johanalcala104@gmail.com>
2019-11-26 20:35:02 -0300

    variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,8 @@
-### ingresar credenciales
+#ingresar credenciales
 variable "access_key" {}
 variable "secret_key" {}
 provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
+  region     = "us-west-2"
 }
